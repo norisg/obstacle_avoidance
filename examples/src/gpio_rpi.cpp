c670d1a42cc95ab13f48bdd64b0a08b7275cd4d9
norisg <>
2021-04-20 19:07:09 +0200

    update

--- examples/src/gpio_rpi.cpp
+++ examples/src/gpio_rpi.cpp
@@ -1,32 +1,11 @@
-#include <iostream>        // Include all needed libraries here
+#include <stdio.h>
 #include <wiringPi.h>
 
-using namespace std;        // No need to keep using “std”
-
-int main()
+int main(int argc, char** argv)
 {
+    wiringPiSetup();
 
-    if(wiringPiSetupGpio()==-1);
-   {
-       cout<<"Setup wiring pi failed";
-       return 1;
-   }           // Setup the library
-    pinMode(11, OUTPUT);        // Configure GPIO11 as an output
-    
-    int dutycycle = 2.5;
-    softPwmCreate(11,0,50);
-    // Main program loop
-    while(1)
-    {
-        if (dutycycle = 2.5) {
-            softPwmWrite(11,dutycycle);
-            dutycycle = 10;
-        }
-        else {
-            softPwmWrite(11,dutycycle);
-            dutycycle = 2.5;
-        }
-        delay(5000);    
-    }
+    printf("wiringPi is working!\n");
+
     return 0;
 }
