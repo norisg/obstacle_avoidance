93407ba81ec4e46f77975b96c2076a4642d90edd
norisg <>
2021-04-20 19:19:11 +0200

    update

--- examples/src/gpio_rpi.cpp
+++ examples/src/gpio_rpi.cpp
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <wiringPi.h>
+#include <softPwm.h>
 
 int main(int argc, char** argv)
 {
     wiringPiSetup();
+    
 
     printf("wiringPi is working!\n");
+    
+    softPwmCreate(11,2.5,50);
+    
 
     return 0;
 }
